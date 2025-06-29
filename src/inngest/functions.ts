e4dee564a8c724a8e67d8d07b548837bd17095da
Aura Cho <acho1833@inst.hcpss.org>
2025-06-29 20:53:40 +0000

    push

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,22 @@
+import { openai, createAgent } from "@inngest/agent-kit";
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
     { id: "hello-world" },
     { event: "test/hello.world" },
     async ({ event, step }) => {
-        await step.sleep("wait-a-moment", "1s");
-        return { message: `Hello ${event.data.email}!` };
+        await step.sleep("wait-a-moment", "5s");
+
+        const codeAgent = createAgent({
+            name: 'code-agent',
+            system: 'You are an expert next.js developer. You write readable, maintainable, and efficient code.  You write simple Next.js & React snippets.',
+            model: openai({ model: 'gpt-4o' })
+        });
+
+        const { output } = await codeAgent.run(
+            `Write the following text: ${event.data.value}`,
+        )
+
+        return { output };
     },
 );
