8187a1578f563c46e3e9423e950a89d4deca137f
Aura Cho <acho1833@inst.hcpss.org>
2025-06-29 22:11:32 +0000

    push

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-2:20
+2:44
+
+Just one time
+e2b template build --name vibe-nextjs-kc-test-2 --cmd "/compile_page.sh"
+
+then just
+e2b template build
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,17 @@
+import { Sandbox } from '@e2b/code-interpreter';
 import { openai, createAgent } from "@inngest/agent-kit";
 import { inngest } from "./client";
+import { getSandbox } from './utils';
 
 export const helloWorld = inngest.createFunction(
     { id: "hello-world" },
     { event: "test/hello.world" },
     async ({ event, step }) => {
+        const sandboxId = await step.run("get-sandbox-id", async () => {
+            const sandbox = await Sandbox.create('vibe-nextjs-kc-test-2');
+            return sandbox.sandboxId;
+        });
+
         // await step.sleep("wait-a-moment", "5s");
 
         const codeAgent = createAgent({
@@ -17,6 +24,12 @@ export const helloWorld = inngest.createFunction(
             `Write the following text: ${event.data.value}`,
         )
 
-        return { output };
+        const sandboxUrl = await step.run("get-sandbox-url", async () => {
+            const sandbox = await getSandbox(sandboxId);
+            const host = sandbox.getHost(3000);
+            return `https://${host}`;
+        });
+
+        return { output, sandboxUrl };
     },
 );
