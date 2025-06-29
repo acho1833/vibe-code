703607453f976c7dd7175efb5c144d1465512496
Aura Cho <acho1833@inst.hcpss.org>
2025-06-29 20:55:15 +0000

    push

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -5,7 +5,7 @@ export const helloWorld = inngest.createFunction(
     { id: "hello-world" },
     { event: "test/hello.world" },
     async ({ event, step }) => {
-        await step.sleep("wait-a-moment", "5s");
+        // await step.sleep("wait-a-moment", "5s");
 
         const codeAgent = createAgent({
             name: 'code-agent',
