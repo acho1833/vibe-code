497ab05bb5483702f29e4a0fd83bc7ec0f1be10e
Aura Cho <acho1833@inst.hcpss.org>
2025-06-29 12:22:35 +0000

    push

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+const Page = () => {
+  return (
+    <div>
+      <Button>
+        Click me
+      </Button>
+    </div>
+  );
+};
+
+export default Page;
