4f862acbc37b03ebc0313c2159bb939dcc39cfca
Aura Cho <acho1833@inst.hcpss.org>
2025-06-29 17:33:37 +0000

    push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,18 @@
-import { caller, getQueryClient, trpc } from '@/trpc/server';
-import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
-import Client from './client';
-import { Suspense } from 'react';
+'use client';
 
-const Page = async () => {
-    const queryClient = getQueryClient();
-    void queryClient.prefetchQuery(trpc.hello.queryOptions({text: 'world'}));    
+import { Button } from "@/components/ui/button";
+import { useTRPC } from "@/trpc/client";
+import { useMutation } from "@tanstack/react-query";
+
+const Page = () => {
+    const trpc = useTRPC();
+    const invoke = useMutation(trpc.invoke.mutationOptions({}));
+
 
     return (
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Client />
-        </Suspense>
-      </HydrationBoundary>
+      <div className="p-4 max-w-7xl mx-auto">
+        <Button onClick={() => invoke.mutate({text: 'John'})}>Invoke Background Job</Button>
+      </div>
     );
 };
 
